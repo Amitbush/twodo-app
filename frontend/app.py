482e0a31e1ac65bffc4cfce0e67c1370117c3bd4
Amitbush <amitbushmits25@gmail.com>
2026-01-20 12:51:40 +0200

    3 tier application

--- frontend/app.py
+++ frontend/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return redirect(url_for('login'))
 
 @app.route('/login')
 def login():
@@ -13,6 +13,10 @@ def login():
 @app.route('/register')
 def register():
     return render_template('register.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
